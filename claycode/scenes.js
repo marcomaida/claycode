a7e95096d516f452b8ec7c3698ac0cee6d8b902e
Marco <mmaidacs@gmail.com>
2023-07-18 22:39:05 +0100

    Refactor scene names, scene polygon

--- claycode/scenes.js
+++ claycode/scenes.js
@@ -1,3 +1,3 @@
-import { } from "./scenes/polygon.js"
-// import { } from "./scenes/static_double.js"
-//import { } from "./scenes/shape_shift.js"
+import { } from "./scenes/scene_polygon.js"
+// import { } from "./scenes/scene_static_double.js"
+//import { } from "./scenes/scene_shape_shift.js"
