9b070ddf3afda3da2e5a53aa6a34fc882692bd4f
Marco <marco@wayve.ai>
2023-08-07 17:01:47 +0100

    Fix scene

--- claycode/scenes/scene_static_double.js
+++ claycode/scenes/scene_static_double.js
@@ -2,8 +2,7 @@ import { } from "../geometry/vector.js"
 import { } from "../geometry/math.js"
 import { clearDrawing, initDrawing } from "../drawing/draw.js"
 import { drawClaycode } from "../drawing/draw_rectangle_claycode.js"
-import { bitsToTree } from "../conversion/converter.js"
-import { BitStreamText } from "../conversion/bit_stream.js"
+import { textToTree } from "../conversion/convert.js";
 
 const app = new PIXI.Application({
     width: window.innerWidth,
@@ -20,8 +19,7 @@ document.body.appendChild(app.view)
 
 function doubleView() {
     var inputText = document.getElementById("inputText").value
-    var stream = new BitStreamText(inputText)
-    let current_tree = bitsToTree(stream)
+    let current_tree = textToTree(inputText)
 
     const window_width = window.innerWidth
     const window_height = window.innerHeight
@@ -34,17 +32,17 @@ function doubleView() {
     var bar_top = window_height / 2 - bar_height / 1.2
 
     const code_frame_square = [new PIXI.Vec(bar_left, bar_top),
-        new PIXI.Vec(bar_left + bar_width, bar_top),
-        new PIXI.Vec(bar_left + bar_width, bar_top + bar_height),
-        new PIXI.Vec(bar_left, bar_top + bar_height)]
+    new PIXI.Vec(bar_left + bar_width, bar_top),
+    new PIXI.Vec(bar_left + bar_width, bar_top + bar_height),
+    new PIXI.Vec(bar_left, bar_top + bar_height)]
 
-    bar_top += bar_height+15
+    bar_top += bar_height + 15
     bar_height /= 2.5
 
     const code_frame_rect = [new PIXI.Vec(bar_left, bar_top),
-        new PIXI.Vec(bar_left + bar_width, bar_top),
-        new PIXI.Vec(bar_left + bar_width, bar_top + bar_height),
-        new PIXI.Vec(bar_left, bar_top + bar_height)]
+    new PIXI.Vec(bar_left + bar_width, bar_top),
+    new PIXI.Vec(bar_left + bar_width, bar_top + bar_height),
+    new PIXI.Vec(bar_left, bar_top + bar_height)]
 
     clearDrawing();
     drawClaycode(current_tree.root, code_frame_square)
@@ -53,6 +51,6 @@ function doubleView() {
 
 doubleView()
 inputTextBox.addEventListener('input', doubleView)
-window.onresize = function() {
+window.onresize = function () {
     doubleView()
 }
