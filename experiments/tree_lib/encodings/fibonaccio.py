0b15d18a5e9516d997f99381122a7e391a638ef0
Marco <marco@wayve.ai>
2023-10-31 21:14:35 +0100

    Add fibonaccio encoding

---
+++ experiments/tree_lib/encodings/fibonaccio.py
@@ -0,0 +1,75 @@
+
+from tree_lib.tree import TreeNode
+
+######################
+# Unordered tree encoding
+# Fibonaccio Encoding
+######################
+
+def bit_string_to_number(bits: str):
+    bits = "1" + bits # Add 1 so that any zero on the left is kept
+    return sum(int(c)*(2**i) for i,c in enumerate(bits[::-1]))
+
+def number_to_bit_string(n):
+    assert n > 0
+    return bin(n)[3:] # remove the 0b, then remove the first one
+
+def fib(n):
+    fibn=1
+    fibnm1=0
+    if n == 0:
+        return 0
+    
+    while n>0:
+        t = fibn + fibnm1
+        fibnm1 = fibn
+        fibn = t
+        n -=1
+
+    return fibn
+
+
+# Returns the largest power of `b` contained in `n`
+def largest_fib(n):
+    fibn = 1
+    while fib(fibn) <= n:
+        fibn+=1
+    return fibn-1
+    
+
+def number_to_fib_decomposition(n):
+    dec = []
+    while n > 0:
+        lfib = largest_fib(n)
+        dec += [lfib-1] # Because we remove the first 0,1
+        n-=fib(lfib)
+    return dec
+
+def fib_decomposition_to_number(dec):
+    dec.sort(reverse=True)
+    return sum([fib(num+1) for num in dec]) # Because we removed the first 0,1
+
+def bits_to_tree(bits: str) -> TreeNode: 
+    def populate_tree_of_number(root: TreeNode, n):
+        dec = number_to_fib_decomposition(n)
+        root.children = [TreeNode() for _ in range(len(dec))]
+        for i,c in enumerate(root.children):
+            populate_tree_of_number(c, dec[i])
+
+    root = TreeNode()
+    n = bit_string_to_number(bits)
+    populate_tree_of_number(root, n)
+
+    root.initialize()
+    return root
+
+def tree_to_bits(root: TreeNode) -> str:
+    def tree_to_number(root: TreeNode):
+        if root.children == []:
+            return 0
+        else:
+            dec = [tree_to_number(c) for c in root.children]
+            dec.sort(reverse=True)
+            return fib_decomposition_to_number(dec)
+        
+    return number_to_bit_string(tree_to_number(root))
