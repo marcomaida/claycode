6070f96aa29bac0766b6791ff501f49ff9b48ddc
Marco Perronet <perronet.marco@gmail.com>
2023-11-03 01:28:57 +0000

    Add tuples decoding

--- experiments/tree_lib/encodings/tuples.py
+++ experiments/tree_lib/encodings/tuples.py
@@ -1,6 +1,7 @@
 from tree_lib.tree import TreeNode, equal, wrap
 from tree_lib.bit_stream import bit_stream_literal
 import itertools
+from enum import Enum
 
 ######################
 # Variant of https://luthert.web.illinois.edu/blog/posts/434.html
@@ -14,10 +15,12 @@ def de_interleave(bits: str) -> tuple[str, str]:
 # Fills with zeroes when the bits don't have the same length
 def interleave(bits_odd: str, bits_even: str) -> str:
     assert bits_odd or bits_even
-    assert len(bits_odd) >= len(bits_even)
+    assert is_int(bits_odd) or is_int(bits_even), f"{bits_odd}, {bits_even}"
     bits = ""
     for odd, even in itertools.zip_longest(bits_odd[::-1], bits_even[::-1]):
+        assert odd or even
         even = even if even else "0"
+        odd = odd if odd else "0"
         bits = even + odd + bits
     # Edge case
     if bits[0] == "0":
@@ -53,6 +56,7 @@ def int_binary_to_int(bits: str) -> int:
 #######################
 
 def bits_to_tree(bits: str) -> TreeNode | None:
+    assert bits
     for bit in bits:
         assert bit == "1" or bit == "0"
     
@@ -89,8 +93,111 @@ def bits_to_tree(bits: str) -> TreeNode | None:
 
         return new_node
     
-    bits = "1" + bits
-    root = rec(bits)
+    if bits == "0":
+        root = None
+    elif bits == "1":
+        root = TreeNode()
+    else:
+        root = rec("1" + bits)
+    
     if root:
         root.initialize()
     return root
+
+#######################
+
+class SortingPolicy(Enum):
+    ODD_LEQ_EVEN = 0
+    ODD_GT_EVEN = 1
+    ODD_GT_EVEN_EQ_ZERO = 2
+    UNSORTED = 3
+
+def get_sorting_order(node: TreeNode) -> SortingPolicy:
+    if not node:
+        return SortingPolicy.UNSORTED
+    if len(node.children) <= 2:
+        return SortingPolicy.ODD_LEQ_EVEN
+    if len(node.children) == 3:
+        return SortingPolicy.ODD_GT_EVEN_EQ_ZERO
+    if len(node.children) == 4:
+        return SortingPolicy.ODD_GT_EVEN
+    raise Exception("Hallooo?!")
+
+# Returns non-marker children and ensures that the node has exactly two children (even null)
+def remove_markers_add_empty_children(node: TreeNode) -> list[TreeNode]:
+    if not node.children:
+        return [None, None]
+    
+    real_children = []
+    if len(node.children) <= 2:
+        real_children = node.children
+    else:
+        # Remove markers by ignoring any two leaves arbitrarily
+        leaves_found = 0
+        for child in node.children:
+            if not child.children and leaves_found < 2:
+                leaves_found += 1
+                continue
+            real_children.append(child)
+
+    # There always has to be two children
+    if len(real_children) == 1:
+        real_children.append(None)
+    assert len(real_children) == 2
+    return real_children
+
+def sort(bits_a: str, bits_b: str, order: SortingPolicy) -> tuple[int]:
+    a = int_binary_to_int(bits_a)
+    b = int_binary_to_int(bits_b)
+
+    if order == SortingPolicy.ODD_LEQ_EVEN:
+        sorted = (bits_a, bits_b) if a <= b else (bits_b, bits_a)
+    elif order == SortingPolicy.ODD_GT_EVEN:
+        sorted = (bits_a, bits_b) if a > b else (bits_b, bits_a)
+        assert a != b
+        if a > b:
+            assert b > 0
+        else:
+            assert a > 0
+    elif order == SortingPolicy.ODD_GT_EVEN_EQ_ZERO:
+        sorted = (bits_a, bits_b) if a > b else (bits_b, bits_a)
+        if a > b:
+            assert b == 0
+        else:
+            assert a == 0
+    else:
+        raise Exception("Rotes licht jaaa?")
+    
+    return sorted
+
+def tree_to_bits(root: TreeNode | None) -> str:
+    if not root:
+        return "0"
+    if len(root.children) == 0:
+        return "1"
+
+    # Always returns bits representing an integer
+    def tau(node) -> str:
+        if not node:
+            return "0"
+        if not node.children:
+            return "1"
+            
+        order = get_sorting_order(node)
+        children = remove_markers_add_empty_children(node)
+        
+        odd_bits, even_bits = sort(tau(children[0]), tau(children[1]), order)
+
+        int_bits = interleave(odd_bits, even_bits)
+        assert is_int(int_bits), int_bits
+
+        return int_bits
+
+    bits = tau(root)
+
+    # Strip initial 1
+    assert len(bits) > 1, bits
+    assert bits[0] == "1", bits
+    bits = bits[1:]
+
+    return bits
