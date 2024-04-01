4788a57323f706912a6a90fbd88d81bb332e17b5
Marco Perronet <perronet.marco@gmail.com>
2024-04-01 18:32:51 +0100

    Add ;

--- generator/conversion/convert.js
+++ generator/conversion/convert.js
@@ -1,28 +1,28 @@
 import * as text_bit_UTF8 from "./text_bits/text_bits_UTF8.js";
 import * as square from "./bits_tree/square.js";
 
-const PREFIX_BITS = [0, 0, 0, 0]
-const SUFFIX_BITS = [0, 0, 0, 0]
+const PREFIX_BITS = [0, 0, 0, 0];
+const SUFFIX_BITS = [0, 0, 0, 0];
 
 function addSyncPattern(bits) {
-  bits.unshift(...PREFIX_BITS)
-  bits.push(...SUFFIX_BITS)
-  return bits
+  bits.unshift(...PREFIX_BITS);
+  bits.push(...SUFFIX_BITS);
+  return bits;
 }
 
 function removeSyncPattern(bits) {
-  let pre = PREFIX_BITS.length
-  let post = SUFFIX_BITS.length
+  let pre = PREFIX_BITS.length;
+  let post = SUFFIX_BITS.length;
   
   if (
     bits.length < PREFIX_BITS.length + SUFFIX_BITS.length ||
     (pre > 0 && bits.slice(0, pre).toString() != PREFIX_BITS.toString()) ||
     (post > 0 && bits.slice(-post, bits.length).toString() != SUFFIX_BITS.toString())
   ) {
-    throw Error(`Sync pattern not present in bits ${bits}`)
+    throw Error(`Sync pattern not present in bits ${bits}`);
   }
 
-  return bits.slice(pre, post > 0 ? -post : bits.length)
+  return bits.slice(pre, post > 0 ? -post : bits.length);
 }
 
 /* This is the external interface of the conversion functions.
