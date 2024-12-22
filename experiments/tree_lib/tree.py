f6eb42eea06f3b4129f2371c10170561ebcb1654
Marco <marco@wayve.ai>
2024-12-22 11:36:55 +0100

    Abe

--- experiments/tree_lib/tree.py
+++ experiments/tree_lib/tree.py
@@ -6,7 +6,7 @@ class TreeNode:
 		self.children = children 
 		self.n_descendants = None
 		self.longest_descending_path = None
-		self.weight = None
+		self.footprint = None
 		self.father_thickness = 1
 
 	def _compute_descendants(self):
@@ -20,20 +20,20 @@ class TreeNode:
 							  for c in self.children], default=0) + 1
 		return self.longest_descending_path
 
-	def _compute_weight(self):
-		self.weight = 1 + sum([c._compute_weight() for c in self.children], 0)
-		return self.weight
+	def _compute_footprint(self):
+		self.footprint = 1 + sum([c._compute_footprint() for c in self.children], 0)
+		return self.footprint
 	
 	def initialize(self):
 		# compute metadata in each this node and children
 		self._compute_descendants()
 		self._compute_longest_descending_path()
-		self._compute_weight()
-  		# self.weight = sum ((node.longest_descending_path 
+		self._compute_footprint()
+  		# self.footprint = sum ((node.longest_descending_path 
 					  		# for node in iterate_tree_depth_first(self)))
 
-	def get_total_weight(self):
-		return sum (node.weight for node in iterate_tree_depth_first(self))
+	def get_total_footprint(self):
+		return sum (node.footprint for node in iterate_tree_depth_first(self))
 		
 	def __repr__(self) -> str:
 		return f"{str(self.children)}"
