a26dcf50a137dcf2825cdd590b2f7f1e32c90bd2
Marco <marco@wayve.ai>
2025-01-15 21:07:53 +0000

    Tweak N, add artifact

--- other/evaluation/generate_test_cases.py
+++ other/evaluation/generate_test_cases.py
@@ -9,7 +9,7 @@ from itertools import product
 
 # Rotations will be generated in the range [-ROTATION_MIN_MAX, ROTATION_MIN_MAX]
 ROTATION_MIN_MAX = 20 
-NUMBER_OF_TEST_CASES_PER_EXPERIMENT=3
+NUMBER_OF_TEST_CASES_PER_EXPERIMENT=1
 
 # Min and max frequency of the sin wave that deforms the code
 MIN_WAVE_FREQUENCY = 1
