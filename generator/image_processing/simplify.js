70bd6fe2723658606feb8e89d423c4ef9ce0fd6d
Marco <marco@wayve.ai>
2025-06-08 17:41:14 +0200

    Remove double license

--- generator/image_processing/simplify.js
+++ generator/image_processing/simplify.js
@@ -1,11 +1,3 @@
-/*!
- * Copyright (c) 2025 Claycode
- * Licensed under the MIT License with the Commons Clause.
- * Commercial use is prohibited without a separate license.
- * See LICENSE in the project root for details.
- * SPDX-License-Identifier: MIT AND Commons-Clause
- */
-
 /*
  (c) 2017, Vladimir Agafonkin
  Simplify.js, a high-performance JS polyline simplification library
